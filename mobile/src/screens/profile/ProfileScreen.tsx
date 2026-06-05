a83eb98acd164265a512444bbc9105f78a358520
ibnelfadilaymenpro-lab <aymenfadil264@gmail.com>
2026-06-05 12:51:29 +0200

    fix: import IcPlay manquant dans ProfileScreen

--- mobile/src/screens/profile/ProfileScreen.tsx
+++ mobile/src/screens/profile/ProfileScreen.tsx
@@ -14,7 +14,7 @@ import { api } from '../../api/client';
 import { RootStackParamList } from '../../navigation';
 import { COLORS, FONT, SPACING, RADIUS, SHADOW } from '../../constants/theme';
 import { useTheme } from '../../hooks/useTheme';
-import { IcSettings, IcSave, IcCheck, IcHeart, IcGrid, IcEdit, IcCamera, IcChart } from '../../components/ui/Icons';
+import { IcSettings, IcSave, IcCheck, IcHeart, IcGrid, IcEdit, IcCamera, IcChart, IcPlay } from '../../components/ui/Icons';
 import { EditProfileScreen } from './EditProfileScreen';
 
 type Nav = NativeStackNavigationProp<RootStackParamList>;
